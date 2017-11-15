b5f2e968acebc5023717f0df459bba91e47dd83d
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-15 13:57:29 -0300

    criando um servidor web

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 'use strict'
 
-console.log('Testando...');
+const http = require('http');
+const debug = require('debug')('baltastr:server');
+const express = require('express');
+
+const app = express();
+const port = 3001;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: 'Node Store API',
+        version: '0.0.1'
+    });
+});
+
+app.use('/', route);
