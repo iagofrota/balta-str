344cbb066b70856b78b9973ef78fbb3682ea3071
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-15 21:22:12 -0300

    adicionei a connection string do mongo

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+// Conecta ao banco
+mongoose.connect('mongodb://iagofrota:iagofrota@ds163705.mlab.com:63705/balta-store')
 
 // Carrega as rotas
 const indexRoute = require('./routes/index-route');
