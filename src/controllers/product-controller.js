974e2c20e97dd99a386627bf7bb2bc0cee5f670e
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-16 21:25:43 -0300

    criando uma função para listar produto

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,30 +3,42 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
-exports.post = (req, res, next) => {
-    let product = new Product(req.body);
-    product
-        .save()
-        .then(x => {
-            res.status(200).send({
-                message: 'Produto cadastrado com sucesso'
-            });
+exports.get = (req, res, next) => {
+    Product
+        .find({
+            active: true
+        }, 'title slug price')
+        .then(data => {
+            res.status(200).send(data);
         }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+    exports.post = (req, res, next) => {
+        let product = new Product(req.body);
+        product
+            .save()
+            .then(x => {
+            res.status(200).send({
+            message: 'Produto cadastrado com sucesso'
+        });
+    }).catch(e => {
             res.status(400).send({
-                message: 'Falha ao cadastrar um produto',
-                data: e
-            });
-        }); 
-};
-
-exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({
-        id: id,
-        item: req.body
+            message: 'Falha ao cadastrar um produto',
+            data: e
+        });
     });
-};
+    };
 
-exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
-};
+    exports.put = (req, res, next) => {
+        const id = req.params.id;
+        res.status(201).send({
+            id: id,
+            item: req.body
+        });
+    };
+
+    exports.delete = (req, res, next) => {
+        res.status(200).send(req.body);
+    };
