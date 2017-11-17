f104701d27c692bc4ce0b07dd4c6298a4a8bec2f
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-16 21:43:06 -0300

    listando produto por id

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -15,30 +15,53 @@ exports.get = (req, res, next) => {
         });
 }
 
-    exports.post = (req, res, next) => {
-        let product = new Product(req.body);
-        product
-            .save()
-            .then(x => {
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description slug price tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getById = (req, res, next) => {
+    Product
+        .findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.post = (req, res, next) => {
+    let product = new Product(req.body);
+    product
+        .save()
+        .then(x => {
             res.status(200).send({
-            message: 'Produto cadastrado com sucesso'
-        });
-    }).catch(e => {
+                message: 'Produto cadastrado com sucesso'
+            });
+        }).catch(e => {
             res.status(400).send({
-            message: 'Falha ao cadastrar um produto',
-            data: e
+                message: 'Falha ao cadastrar um produto',
+                data: e
+            });
         });
+};
+
+exports.put = (req, res, next) => {
+    const id = req.params.id;
+    res.status(201).send({
+        id: id,
+        item: req.body
     });
-    };
+};
 
-    exports.put = (req, res, next) => {
-        const id = req.params.id;
-        res.status(201).send({
-            id: id,
-            item: req.body
-        });
-    };
-
-    exports.delete = (req, res, next) => {
-        res.status(200).send(req.body);
-    };
+exports.delete = (req, res, next) => {
+    res.status(200).send(req.body);
+};
